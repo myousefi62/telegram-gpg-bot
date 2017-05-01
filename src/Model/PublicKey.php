c840ee6cdd4dc9910442d6f1e20629c4077f4b61
iillexial <iillexial@gmail.com>
2017-05-01 22:04:58 +0300

    Added import keys function

---
+++ src/Model/PublicKey.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class PublicKey
+ *
+ * @package App\Model
+ * @author Ivan Klymenchenko <iillexial@gmail.com>
+ */
+class PublicKey extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'public_keys';
+}
--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,29 @@
 
 $app->post('/hook', function ($request, $response, $args) {
     $this->logger->info("Telegram hook received");
+
+});
+
+$app->post('/getUpdates', function ($request, $response, $args) {
     /** @var \Telegram\Bot\Api $telegram */
     $telegram = $this->telegram;
+    $response = $telegram->getWebhookUpdates();
+
+    $gpg = new \gnupg();
+
+    $response->each(function ($message) use ($gpg, $telegram) {
+
+        if ($gpg->addencryptkey($message['message']['text'])) {
+            $model = new \App\Model\PublicKey();
+            $model->user_id = $message['message']['from']['id'];
+            $model->public_key = $message['message']['text'];
+            $model->save();
+            $telegram->sendMessage(['text' => 'Success']);
+
+        } else {
+            $telegram->sendMessage(['text' => 'Error']);
+        }
+
+    });
+
 });
