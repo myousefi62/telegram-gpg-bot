154693c8b1d73e4124738b6b009e46610d7ac62f
iillexial <iillexial@gmail.com>
2017-05-06 13:44:56 +0300

    ...

--- src/routes.php
+++ src/routes.php
@@ -5,14 +5,24 @@ $app->post('/hook', function ($request, $response, $args) {
     $this->logger->info("Telegram hook received");
     /** @var \Telegram\Bot\Api $telegram */
     $telegram = $this->telegram;
-    /** @var \Telegram\Bot\Objects\Update $update */
+
     $update = $telegram->getWebhookUpdates();
 
-    $publicKeyModel = \App\Model\PublicKey::firstOrNew([
+    $publicKeyModel = new \App\Model\PublicKey();
+    $publicKeyModel->fill([
         'user_id' => $update->getMessage()->getFrom()->getId(),
         'public_key' => $update->getMessage()->getText()
     ]);
+
+    if ($publicKeyModel->exists) {
+        $publicKeyModel->update();
+    } else {
+        $publicKeyModel->save();
+    }
+
     $publicKeyModel->save();
+
     $telegram->sendMessage(['text' => 'Success', 'chat_id' => $update->getMessage()->getChat()->getId()]);
+
 });
 
