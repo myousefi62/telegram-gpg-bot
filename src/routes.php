91f1a5739633619487931191cc2e9bc9159e30b6
iillexial <iillexial@gmail.com>
2017-05-06 13:37:13 +0300

    Refactor save method

--- src/routes.php
+++ src/routes.php
@@ -5,12 +5,14 @@ $app->post('/hook', function ($request, $response, $args) {
     $this->logger->info("Telegram hook received");
     /** @var \Telegram\Bot\Api $telegram */
     $telegram = $this->telegram;
-
+    /** @var \Telegram\Bot\Objects\Update $update */
     $update = $telegram->getWebhookUpdates();
-    $model = new \App\Model\PublicKey();
-    $model->user_id = $update->getMessage()->getFrom()->getId();
-    $model->public_key = $update->getMessage()->getText();
-    $model->save();
+
+    $publicKeyModel = \App\Model\PublicKey::firstOrNew([
+        'user_id' => $update->getMessage()->getFrom()->getId(),
+        'public_key' => $update->getMessage()->getText()
+    ]);
+    $publicKeyModel->save();
     $telegram->sendMessage(['text' => 'Success', 'chat_id' => $update->getMessage()->getChat()->getId()]);
 });
 
